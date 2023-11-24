6e1f7e67e7bed88692c4c5e550a3cee548c29fa4
loukaikai <1650918851@qq.com>
2023-11-24 17:10:12 +0800

    elasticsearch

---
+++ uranusmall-search/src/test/java/com/example/uranusmallsearch/UranusmallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uranusmallsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UranusmallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
